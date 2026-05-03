638c80ee3a7457f89ff151200587a799af86aaad
Andrés Contreras Guillén <ancongui@Andress-MacBook-Pro.local>
2026-05-03 13:04:42 +0200

    chore: rebrand copyright to Firefly Software Foundation
    Update copyright notices, NOTICE/LICENSE references, and source
    comments to reflect the project's release under the Firefly Software
    Foundation per the Apache License 2.0.

--- src/main/java/org/fireflyframework/ecm/adapter/azureblob/AzureBlobDocumentContentAdapter.java
+++ src/main/java/org/fireflyframework/ecm/adapter/azureblob/AzureBlobDocumentContentAdapter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Firefly Software Solutions Inc.
+ * Copyright (c) 2024 Firefly Software Foundation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -61,7 +61,7 @@ import java.util.UUID;
  *
  * <p>Content naming convention: {pathPrefix}/{documentId}.content</p>
  *
- * @author Firefly Software Solutions Inc.
+ * @author Firefly Software Foundation.
  * @version 1.0
  * @since 1.0
  */
